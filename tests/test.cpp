d13271001a7964531331ae24e19a4c9c807fe5a4
Beenv12 <36257286+Beenv12@users.noreply.github.com>
2018-06-10 20:51:54 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,6 +11,7 @@ TEST_CASE("output values should match input values", "[file]")
  
     Tree tree(a);
     Tree tree1({3, 4, 4, 2, 6, 5, 7});
+   Tree();
  
     obj.outStars();
     obj.MakeDecisionTree(1,&tree);
